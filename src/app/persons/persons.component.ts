622f539ec501db2fb326b37b84201198ed54089e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-02-06 19:34:48 +0100

    validation

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Person } from '../shared/classes/person';
 
 @Component({
   selector: 'app-persons',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './persons.component.css'
 })
 export class PersonsComponent {
+  personne= {} as Person;
 
 }
